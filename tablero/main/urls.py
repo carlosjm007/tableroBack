57f580b62eaf74b6d01aa4da7c2bd8ac76976ca4
carlosj.manosalvar@gmail.com <renault4>
2019-03-07 00:37:42 -0500

    Se añade frontend de proyecto

--- tablero/main/urls.py
+++ tablero/main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from .api import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
+from .views import *
 
 urlpatterns = [
 	path('api/auth', include('knox.urls')),
 	path('api/auth/register', RegisterAPI.as_view()),
 	path('api/auth/login', LoginAPI.as_view()),
 	path('api/auth/user', UserAPI.as_view()),
-	path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
+	path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+	path('', index),
 ]
